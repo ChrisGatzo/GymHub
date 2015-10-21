50c48079cac98db97f15040ce7ef3feaa97ec0c9
Chris Gatzonis <gatzonis1988@hotmail.com>
2015-10-21 19:36:38 -0400

    Workout of the day controller API

---
+++ GymHub.WebApi/Controllers/WorkoutOfTheDayController.cs
@@ -0,0 +1,69 @@
+﻿using System.Web.Http;
+using GymHub.Service;
+using GymHub.Service.DataTransferObjects;
+
+namespace GymHub.WebApi.Controllers
+{
+    [RoutePrefix("api/workoutOfTheDay")]
+    public class WorkoutOfTheDayController : ApiController
+    {
+        private readonly IAthleteService _athleteService;
+        private readonly IStatisticsService _statisticsService;
+
+        public WorkoutOfTheDayController(
+            IAthleteService athleteService,
+            IStatisticsService statisticsService)
+        {
+            _athleteService = athleteService;
+            _statisticsService = statisticsService;
+        }
+
+        [Route("InactiveAthletes")]
+        public IHttpActionResult GetInactiveAthletes()
+        {
+            var request = new GetInactiveAthletesRequest();
+
+            var inactiveAthletes =
+                _athleteService
+                .GetInactiveAthletes(request)
+                .InactiveAthletes;
+
+            return Ok(inactiveAthletes);
+        }
+
+        [Route("ActiveAthletes")]
+        public IHttpActionResult GetActiveAthletes()
+        {
+
+            var request = new GetFilteredAthletesRequest
+            {
+                //OrderByColumn = orderByColumn,
+                //OrderDirection = orderDirection,
+                //SearchValue = searchValue,
+                //Start = requestModel.Start,
+                //Length = requestModel.Length,
+                WithExercisesOfTheDay = true,
+                WithStatistics = true
+            };
+
+            var response = _athleteService.GetFilteredAthletes(request);
+
+            return Ok(response);
+        }
+
+        public IHttpActionResult AthleteCheckIn([FromBody]int athleteId)
+        {
+            var request = new CheckInAthleteRequest
+            {
+                AthleteId = athleteId
+            };
+
+            var response = _athleteService.CheckInAthlete(request);
+
+            //TODO: Enable SingnalR.
+            //ActiveAthletesHub.DispatchToClient();
+
+            return Ok("Athlete Checked In Successfully");
+        }
+    }
+}
